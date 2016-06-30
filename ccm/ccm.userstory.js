65e5ad277eeefec37168201037327139cc751315
CrystaX01 <aschwindt@freenet.de>
2016-07-01 00:33:48 +0200

    CCM User Story V1.4
    - BUGFIX: von ID selector auf class gewechselt
    - BUGFIX: es können nurnoch Zahlen eingetragen werden in Storypoint feld
    - Feature: Reihenfolge jetzt änderbar
    - Store: Beispieldaten werden jetzt aus einem lokalen Store geladen
    - Design: Komplett überarbeitetes Design mit Button Animationen und einheitlichen Farb-stil

--- ccm/ccm.userstory.js
+++ ccm/ccm.userstory.js
@@ -23,19 +23,102 @@ ccm.component( {
 					element.html( ccm.helper.html( self.html.get( 'main' ) ) );
 					var userstorys_div = ccm.helper.find( self, '.userstorys' );     // neue private Variable
 					for ( var i = 0; i < dataset.userstorys.length; i++ ) {		// Iterierung
-						var userstory = dataset.userstorys[ i ];
-						userstorys_div.append( ccm.helper.html( self.html.get( 'userstory' ), {
-							id: ccm.helper.val(i+1),
-							text: ccm.helper.val( userstory.text ),
-							points: ccm.helper.val( userstory.points ),
-							bid: ccm.helper.val(i),
-							onclick: function () {
-								dataset.userstorys.splice(this.getAttribute('id'),1);
-								self.store.set(dataset, function (){
-									self.render();
-								});
-							}
-						}));
+						var userstory = dataset.userstorys[i];
+						var last = dataset.userstorys.length-1;
+						if(dataset.userstorys.length==1) { // Wenn es nur einen Datensatz gibt, Userstory ohne arrow-keys ausgeben
+							userstorys_div.append( ccm.helper.html( self.html.get( 'userstory4' ), { // Trage Userstory mit Werten ein
+								id: ccm.helper.val('Userstory ' + (i+1)),
+								text: ccm.helper.val( userstory.text ),
+								points: ccm.helper.val( userstory.points ),
+								bid: ccm.helper.val(i),
+								onclick: function () { // Lösch Funktion
+									dataset.userstorys.splice(this.getAttribute('class'), 1); // Lösche gewählte Userstory aus dataset
+									self.store.set(dataset, function (){ // Update Store und render die ausgabe neu
+										self.render();
+									});
+								}
+							}));
+						} else if(i==0) { // Ausgabe des ersten Datensatzes mit Arrowkey down
+							userstorys_div.append( ccm.helper.html( self.html.get( 'userstory2' ), { // Trage Userstory mit Werten ein
+								id: ccm.helper.val('Userstory ' + (i+1)),
+								text: ccm.helper.val( userstory.text ),
+								points: ccm.helper.val( userstory.points ),
+								bid: ccm.helper.val(i),
+								down: function() { // Key-Down Funktion
+									var sort_id = parseInt(this.getAttribute('class')); //Wähle ID des momentanen Objects
+									var value = dataset.userstorys[sort_id].text; //Speichere Text des zu bewegenden Objects
+									var value2 = dataset.userstorys[sort_id].points; //Speichere Points des zu bewegenden Objects
+									dataset.userstorys.splice(sort_id, 1); //Lösche bewegendes Object
+									dataset.userstorys.splice((sort_id+1), 0, {text: value, points: value2}); //Füge zu bewegendes Object an richtiger stelle ein
+									self.store.set(dataset, function (){ // Update Store und render die ausgabe neu
+										self.render();
+									});
+								},
+								onclick: function () { // Lösch Funktion
+									dataset.userstorys.splice(this.getAttribute('class'), 1); // Lösche gewählte Userstory aus dataset
+									self.store.set(dataset, function (){ // Update Store und render die ausgabe neu
+										self.render();
+									});
+								}
+							}));
+						} else if(i==last) { // Ausgabe des letzten Datensatzes mit Arrowkey up
+							userstorys_div.append( ccm.helper.html( self.html.get( 'userstory3' ), { // Trage Userstory mit Werten ein
+								id: ccm.helper.val('Userstory ' + (i+1)),
+								text: ccm.helper.val( userstory.text ),
+								points: ccm.helper.val( userstory.points ),
+								bid: ccm.helper.val(i),
+								up: function() { // Key-Up Funktion
+									var sort_id = parseInt(this.getAttribute('class')); //Wähle ID des momentanen Objects
+									var value = dataset.userstorys[sort_id].text; //Speichere Text des zu bewegenden Objects
+									var value2 = dataset.userstorys[sort_id].points; //Speichere Points des zu bewegenden Objects
+									dataset.userstorys.splice(sort_id, 1); //Lösche bewegendes Object
+									dataset.userstorys.splice((sort_id-1), 0, {text: value, points: value2}); //Füge zu bewegendes Object an richtiger stelle ein
+									self.store.set(dataset, function (){ // Update Store und render die ausgabe neu
+										self.render();
+									});
+								},
+								onclick: function () { // Lösch Funktion
+									dataset.userstorys.splice(this.getAttribute('class'), 1); // Lösche gewählte Userstory aus dataset
+									self.store.set(dataset, function (){ // Update Store und render die ausgabe neu
+										self.render();
+									});
+								}
+							}));
+						} else { // Standard userstory ausgabe mit beiden Arrowkeys
+							userstorys_div.append( ccm.helper.html( self.html.get( 'userstory' ), { // Trage Userstory mit Werten ein
+								id: ccm.helper.val('Userstory ' + (i+1)),
+								text: ccm.helper.val( userstory.text ),
+								points: ccm.helper.val( userstory.points ),
+								bid: ccm.helper.val(i),
+								up: function() { // Key-Up Funktion
+									var sort_id = parseInt(this.getAttribute('class')); //Wähle ID des momentanen Objects
+									var value = dataset.userstorys[sort_id].text; //Speichere Text des zu bewegenden Objects
+									var value2 = dataset.userstorys[sort_id].points; //Speichere Points des zu bewegenden Objects
+									dataset.userstorys.splice(sort_id, 1); //Lösche bewegendes Object
+									dataset.userstorys.splice((sort_id-1), 0, {text: value, points: value2}); //Füge zu bewegendes Object an richtiger stelle ein
+									self.store.set(dataset, function (){ // Update Store und render die ausgabe neu
+										self.render();
+									});
+								},
+								down: function() { // Key-Down Funktion
+									var sort_id = parseInt(this.getAttribute('class')); //Wähle ID des momentanen Objects
+									var value = dataset.userstorys[sort_id].text; //Speichere Text des zu bewegenden Objects
+									var value2 = dataset.userstorys[sort_id].points; //Speichere Points des zu bewegenden Objects
+									dataset.userstorys.splice(sort_id, 1); //Lösche bewegendes Object
+									dataset.userstorys.splice(sort_id+1, 0, {text: value, points: value2}); //Füge zu bewegendes Object an richtiger stelle ein
+									self.store.set(dataset, function (){ // Update Store und render die ausgabe neu
+										self.render();
+									});
+								},
+								onclick: function () { // Lösch Funktion
+									dataset.userstorys.splice(this.getAttribute('class'), 1); // Lösche gewählte Userstory aus dataset
+									self.store.set(dataset, function (){ // Update Store und render die ausgabe neu
+										self.render();
+									});
+								}
+							}));
+						}
+						
 					}
 					userstorys_div.append( ccm.helper.html( self.html.get( 'input' ), {  // Anhängen des input-Templates
 						onsubmit: function () {						
@@ -54,12 +137,3 @@ ccm.component( {
 		}
 	}
 });
-/*
-function () {
-	dataset.userstorys.splice(i,1);
-	self.store.set(dataset, function (){
-		self.render();
-
-	});
-}
-*/
